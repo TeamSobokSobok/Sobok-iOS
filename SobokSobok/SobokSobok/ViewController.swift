07ba5c78da7568c9357bc06b993bede7405e1559
김태현 <taehyk528@gmail.com>
2022-01-07 03:47:57 +0900

    [Add] 프로젝트 생성 (#3)

---
+++ SobokSobok/SobokSobok/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SobokSobok
+//
+//  Created by taehy.k on 2022/01/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
