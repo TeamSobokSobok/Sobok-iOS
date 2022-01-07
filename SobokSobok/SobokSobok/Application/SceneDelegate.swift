452324feefe2fb366f0b018edac3f6ebcc2c8a60
김태현 <taehyk528@gmail.com>
2022-01-08 02:56:54 +0900

    [Style] 폰트 테스트 (#5)

--- SobokSobok/SobokSobok/Application/SceneDelegate.swift
+++ SobokSobok/SobokSobok/Application/SceneDelegate.swift
@@ -16,8 +16,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
         
-        let splashViewController = SplashViewController()
-        window?.rootViewController = splashViewController
+        let sampleViewController = SampleViewController.instanceFromNib()
+        window?.rootViewController = sampleViewController
         window?.makeKeyAndVisible()
     }
 }
