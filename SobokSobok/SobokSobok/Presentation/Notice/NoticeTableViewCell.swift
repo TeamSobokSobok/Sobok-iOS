15c513d16698c65c1d221195d10ead0e8f4f9636
EunHee-Jeong <18102007@seoultech.ac.kr>
2022-01-10 01:36:01 +0900

    [Style] 테이블뷰 셀 생성 및 UI 구성 (#13)
    - 거절, 수락 버튼 포함

---
+++ SobokSobok/SobokSobok/Presentation/Notice/NoticeTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  NoticeTableViewCell.swift
+//  SobokSobok
+//
+//  Created by 정은희 on 2022/01/09.
+//
+
+import UIKit
+
+class NoticeTableViewCell: UITableViewCell {
+
+    static let cellIdentifier = "NoticeTableViewCell"
+    
+    // MARK: - View Life Cycle
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    // MARK: - Functions
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+    }
+    
+}
