aaa6921e5c2cc4eff2f79cdfb5c00a065c76dc61
EunHee-Jeong <18102007@seoultech.ac.kr>
2022-01-09 22:59:02 +0900

    [Add] 컨테이너 뷰 생성 및 오토레이아웃 (#13)
    - 컨테이너 뷰 파일 생성
    - 오토레이아웃 구성

---
+++ SobokSobok/SobokSobok/Presentation/Notice/NoticeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  NoticeViewController.swift
+//  SobokSobok
+//
+//  Created by 정은희 on 2022/01/09.
+//
+
+import UIKit
+
+class NoticeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
