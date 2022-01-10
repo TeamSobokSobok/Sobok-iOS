fcb78c12e48741300607804d4955aa875738785f
EunHee-Jeong <18102007@seoultech.ac.kr>
2022-01-10 13:11:26 +0900

    [Feat] 알림탭 첫번째 화면 구현 (#13)
    - 셀 디폴트 크기 134로 임시 설정

--- SobokSobok/SobokSobok/Presentation/Notice/NoticeTableViewCell.swift
+++ SobokSobok/SobokSobok/Presentation/Notice/NoticeTableViewCell.swift
@@ -7,9 +7,14 @@
 
 import UIKit
 
-class NoticeTableViewCell: UITableViewCell {
+final class NoticeTableViewCell: UITableViewCell {
 
     static let cellIdentifier = "NoticeTableViewCell"
+    
+    // MARK: - @IBOutlet Properties
+    @IBOutlet var noticeImage: UIImageView!
+    @IBOutlet var noticeStatus: UILabel!
+    @IBOutlet var noticeTime: UILabel!
     
     // MARK: - View Life Cycle
     override func awakeFromNib() {
@@ -19,7 +24,23 @@ class NoticeTableViewCell: UITableViewCell {
     // MARK: - Functions
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
     }
     
+    func setData(noticeData: NoticeListData) {
+        noticeImage.image = noticeData.makeNoticeImage()
+        noticeStatus.text = noticeData.noticeTitle
+        noticeTime.text = noticeData.noticeTime
+    }
+}
+
+// MARK: - Extensions
+extension UIView {
+    @IBInspectable var cornerRadius: CGFloat {
+        set {
+            layer.cornerRadius = newValue
+        }
+        get {
+            return layer.cornerRadius
+        }
+    }
 }
--- SobokSobok/SobokSobok/Presentation/Notice/NoticeViewController.swift
+++ SobokSobok/SobokSobok/Presentation/Notice/NoticeViewController.swift
@@ -7,23 +7,62 @@
 
 import UIKit
 
-class NoticeViewController: UIViewController {
+final class NoticeViewController: UIViewController {
 
+    // MARK: - @IBOutlet Properties
+    @IBOutlet var notificationTableView: UITableView!
+    
+    // MARK: - Properties
+    var noticeList: [NoticeListData] = []
+    
+    // MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        assignDelegation()
+        registerXib()
+        initNoticeList()
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    // MARK: - Functions
+    func assignDelegation() {
+        notificationTableView.delegate = self
+        notificationTableView.dataSource = self
     }
-    */
-
+    
+    func registerXib() {
+        notificationTableView.register(UINib(nibName: NoticeTableViewCell.cellIdentifier, bundle: nil), forCellReuseIdentifier: NoticeTableViewCell.cellIdentifier)
+    }
+    
+    func initNoticeList() {
+        noticeList.append(contentsOf: [
+            NoticeListData(noticeImageName: "icCalendarAlarmMint", noticeTitle: "지민지민님이 캘린더 공유를 요청했어요", noticeTime: "오후 12:35"),
+            NoticeListData(noticeImageName: "icFillAlarmMint", noticeTitle: "수현이님이 복약 정보를 전송했어요", noticeTime: "오전 10:40"),
+            NoticeListData(noticeImageName: "icFillAlarmGray", noticeTitle: "효영님의 약 전송을 거절했어요", noticeTime: "오전 8:21"),
+            NoticeListData(noticeImageName: "icFillAlarmMint", noticeTitle: "나는야효영이라네로를님이 복약 정보를 전송했어요", noticeTime: "2022.01.06"),
+            NoticeListData(noticeImageName: "icFillAlarmGray", noticeTitle: "나는야효영이라네로를님의 약 전송을 거절했어요", noticeTime: "2022.01.06")
+        ])
+    }
 }
+
+// MARK: - Extensions
+extension NoticeViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return noticeList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = notificationTableView.dequeueReusableCell(withIdentifier: NoticeTableViewCell.cellIdentifier) as? NoticeTableViewCell else {return UITableViewCell()}
+        cell.selectionStyle = .none
+        cell.setData(noticeData: noticeList[indexPath.row])
+        return cell
+    }
+}
+
+extension NoticeViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 134
+//        return notificationTableView.rowHeight    // 셀 크기 동적으로 조절 시도...
+    }
+}
+
