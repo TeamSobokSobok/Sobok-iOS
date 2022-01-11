8b10bdff6cd7e8ca4c3a3cded2d68d87bdd947dd
seondal <sseondal@gmail.com>
2022-01-11 15:13:30 +0900

    [Add] SignUpViewController생성 (#20)

---
+++ SobokSobok/SobokSobok/Presentation/SignUp/SignUpViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SignUpViewController.swift
+//  SobokSobok
+//
+//  Created by 김선오 on 2022/01/11.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
