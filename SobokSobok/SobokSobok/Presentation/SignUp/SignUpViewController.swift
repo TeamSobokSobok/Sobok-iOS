e6ded538840fd6be3aae1689d685ec7640c673d3
seondal <sseondal@gmail.com>
2022-01-11 18:09:56 +0900

    [Feat] 회원가입뷰 세개의 텍스트필드를 추가해야만 버튼 활성화 (#20)

--- SobokSobok/SobokSobok/Presentation/SignUp/SignUpViewController.swift
+++ SobokSobok/SobokSobok/Presentation/SignUp/SignUpViewController.swift
@@ -9,10 +9,35 @@ import UIKit
 
 final class SignUpViewController: BaseViewController {
 
+    @IBOutlet weak var okayButton: UIButton!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var rePasswordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        checkTextField()
     }
 
+    private func checkTextField(){
+        okayButton.isEnabled = false
+        
+        self.emailTextField.addTarget(self, action: #selector(self.activateOkayButton(_:)), for: .editingChanged)
+        self.passwordTextField.addTarget(self, action: #selector(self.activateOkayButton(_:)), for: .editingChanged)
+        self.rePasswordTextField.addTarget(self, action: #selector(self.activateOkayButton(_:)), for: .editingChanged)
+
+    }
+    
+    @objc func activateOkayButton(_ : UIButton){
+        let emailFilled = emailTextField.hasText
+        let passwordFilled = passwordTextField.hasText
+        let rePasswordFilled = rePasswordTextField.hasText
+        
+        if emailFilled && passwordFilled && rePasswordFilled {
+            okayButton.isEnabled = true
+        } else {
+            okayButton.isEnabled = false
+        }
+
+    }
 }
