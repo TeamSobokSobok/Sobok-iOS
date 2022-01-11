bf07ec44dfab0cb4564daef69a9362dbba40540d
김태현 <taehyk528@gmail.com>
2022-01-11 19:52:56 +0900

    [Style] MedicineCollectionViewCell (#28)

---
+++ SobokSobok/SobokSobok/Presentation/Home/MedicineCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  MedicineCollectionViewCell.swift
+//  SobokSobok
+//
+//  Created by taehy.k on 2022/01/11.
+//
+
+import UIKit
+
+final class MedicineCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
