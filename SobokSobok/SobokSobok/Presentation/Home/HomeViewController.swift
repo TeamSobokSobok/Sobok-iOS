dd228a45cce0bd1df106d6e3a8461b1e54e683d1
김태현 <taehyk528@gmail.com>
2022-01-11 18:36:27 +0900

    [Add] HomeViewController (#28)

---
+++ SobokSobok/SobokSobok/Presentation/Home/HomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  HomeViewController.swift
+//  SobokSobok
+//
+//  Created by taehy.k on 2022/01/11.
+//
+
+import UIKit
+
+final class HomeViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
