3899164803e4dfad8184e5abeebf48a7f0b04bd8
EunHee-Jeong <18102007@seoultech.ac.kr>
2022-01-11 14:04:40 +0900

    [Add] NavigationBarViewController 파일 생성
    - UIViewController, xib

---
+++ SobokSobok/SobokSobok/Presentation/Common/NavigationBarViewController.swift
@@ -0,0 +1,16 @@
+//
+//  NavigationBarViewController.swift
+//  SobokSobok
+//
+//  Created by 정은희 on 2022/01/11.
+//
+
+import UIKit
+
+class NavigationBarViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
