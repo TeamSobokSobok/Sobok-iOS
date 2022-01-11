dce7bcea4d167357c4830ca4bf6fd71df670846a
EunHee-Jeong <18102007@seoultech.ac.kr>
2022-01-11 17:59:11 +0900

    [Fix] 클래스 변경 (#22)
    - UIViewController ➡️ UIView

--- SobokSobok/SobokSobok/Presentation/Common/NavigationBarView.swift
+++ SobokSobok/SobokSobok/Presentation/Common/NavigationBarView.swift
@@ -1,5 +1,5 @@
 //
-//  NavigationBarViewController.swift
+//  NavigationBarView.swift
 //  SobokSobok
 //
 //  Created by 정은희 on 2022/01/11.
@@ -7,15 +7,10 @@
 
 import UIKit
 
-class NavigationBarViewController: UIViewController {
-    
+class NavigationBarView: UIView {
+
     // MARK: - @IBOutlet Properties
     @IBOutlet weak var previousButton: UIButton!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var nextButton: UILabel!
-    
-    // MARK: - View Life Cycle
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
 }
