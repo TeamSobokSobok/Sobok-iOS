39e29e7cd320169b183a398959ceeeb795e74b72
EunHee-Jeong <18102007@seoultech.ac.kr>
2022-01-11 18:14:36 +0900

    [Add] 컨테이너 뷰 파일 생성 (#25)

---
+++ SobokSobok/SobokSobok/Presentation/Notice/SendInfoViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SendInfoViewController.swift
+//  SobokSobok
+//
+//  Created by 정은희 on 2022/01/11.
+//
+
+import UIKit
+
+class SendInfoViewController: UIViewController {
+
+    // MARK: - View Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
