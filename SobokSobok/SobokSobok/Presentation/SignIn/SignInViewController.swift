9c3e835ec00df49244b92fa4381c3acd57961ec2
seondal <sseondal@gmail.com>
2022-01-11 17:59:32 +0900

    [Feat] 이메일 비밀번호가 입력이 되어있어야만 로그인 버튼 활성화 (#20)

--- SobokSobok/SobokSobok/Presentation/SignIn/SignInViewController.swift
+++ SobokSobok/SobokSobok/Presentation/SignIn/SignInViewController.swift
@@ -11,17 +11,38 @@ final class SignInViewController: BaseViewController {
 
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var logInButton: UIButton!
+    @IBOutlet weak var signUpButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        checkTextField()
     }
     
-    @IBAction func toucuUpToSetAutoLogIn(_ sender: Any) {
-        print("자동로그인")
+    private func checkTextField() {
+        logInButton.isEnabled = false
+        signUpButton.isEnabled = true
+        
+        self.emailTextField.addTarget(self, action: #selector(self.activateLogInButton(_:)),for:.editingChanged)
+        self.passwordTextField.addTarget(self, action: #selector(self.activateLogInButton(_:)), for: .editingChanged)
     }
+    
+    @objc func activateLogInButton(_ : UIButton){
+        
+        let emailFilled = emailTextField.hasText
+        let passwordFilled = passwordTextField.hasText
+        
+        if emailFilled && passwordFilled {
+            logInButton.isEnabled = true
+        } else {
+            logInButton.isEnabled = false
+        }
+    }
+    
     @IBAction func touchUpToLogin(_ sender: Any) {
         print("이메일:\(emailTextField.text),  비번:\(passwordTextField.text)")
     }
+    
     @IBAction func touchUpToMoveToSignUpView(_ sender: Any) {
         print("회원가입")
         navigationController?.pushViewController(SignUpViewController.instanceFromNib(), animated: true)
