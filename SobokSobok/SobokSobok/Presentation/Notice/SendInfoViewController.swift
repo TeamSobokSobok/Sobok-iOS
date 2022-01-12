8553c2d18c30f8b55442175b7f3a7ec0eda680b3
EunHee-Jeong <18102007@seoultech.ac.kr>
2022-01-12 23:45:19 +0900

    [Edit] 상단바 UI 수정 (#25)

--- SobokSobok/SobokSobok/Presentation/Notice/SendInfoViewController.swift
+++ SobokSobok/SobokSobok/Presentation/Notice/SendInfoViewController.swift
@@ -17,15 +17,11 @@ final class SendInfoViewController: UIViewController {
         $0.backgroundColor = .white
     }
     let navigationTitleLabel = UILabel().then {
-        $0.text = "내 약 추가하기"
+        $0.text = "전송 받은 약"
         $0.font = .systemFont(ofSize: 17)
     }
     let xButton = UIButton().then {
         $0.setImage(Image.icClose48, for: .normal)
-    }
-    let nextButton = UIButton().then {
-        $0.setTitle("다음", for: .normal)
-        $0.setTitleColor(Color.mint, for: .normal)
     }
     private var sendInfoList: [SendInfoListData] = SendInfoListData.dummy
     
@@ -46,7 +42,7 @@ final class SendInfoViewController: UIViewController {
     
     // MARK: - Functions
     private func setUI() {
-        [navigationView, navigationTitleLabel, xButton, nextButton].forEach {
+        [navigationView, navigationTitleLabel, xButton].forEach {
             view.addSubview($0)
         }
         [refuseButton, acceptButton].forEach {
@@ -69,14 +65,6 @@ final class SendInfoViewController: UIViewController {
             $0.leading.equalTo(navigationView).inset(1)
             $0.centerY.equalTo(navigationTitleLabel)
         }
-        
-        nextButton.snp.makeConstraints {
-            $0.trailing.equalTo(navigationView.snp.trailing).inset(20)
-            $0.centerY.equalTo(navigationTitleLabel)
-            $0.height.equalTo(44)
-            $0.width.equalTo(44)
-        }
-        
     }
     
     func assignDelegation() {
