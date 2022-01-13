7bde694b46264c742bb2f1b82edec803a54e6cb1
seondal <sseondal@gmail.com>
2022-01-14 06:04:57 +0900

    [Edit] 함수 순서 정리 및 주석 추가 (#38)

--- SobokSobok/SobokSobok/Presentation/SignUp/SignUpViewController.swift
+++ SobokSobok/SobokSobok/Presentation/SignUp/SignUpViewController.swift
@@ -48,11 +48,13 @@ final class SignUpViewController: BaseViewController {
     
     // MARK: Functions
     
+    // 비밀번호 숨기기
     private func securePassword() {
         passwordTextField.isSecureTextEntry = true
         rePasswordTextField.isSecureTextEntry = true
     }
     
+    // 텍스트필드 addTarget
     private func checkTextField() {
         emailWarningStackView.isHidden = true
         emailTextField.addTarget(self, action: #selector(self.checkEmailTextField), for: .editingChanged)
@@ -60,25 +62,27 @@ final class SignUpViewController: BaseViewController {
         rePasswordTextField.addTarget(self, action: #selector(self.checkRepasswordTextField), for: .editingChanged)
     }
     
-    @objc private func checkEmailTextField() {
-        emailRight = checkEmailRight(input: emailTextField.text ?? "")
-        emailWarningStackView.isHidden = emailRight || !emailTextField.hasText ? true : false
-    }
-    
+    // 정규식 체크
     private func checkEmailRight (input: String) -> Bool {
         // 이메일 조건 : [대문자,소문자,숫자,특수기호] + 골뱅이(@) + [대문자,소문자,숫자,.,-] + 점(.) + [대문자,소문자]
         let validEmail = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
         let emailTest = NSPredicate(format: "SELF MATCHES %@", validEmail)
           return emailTest.evaluate(with: input)
     }
-    
-    @objc private func checkPasswordTextField() {
-        
+    private func checkPasswordRight (input: String) -> Bool {
+        // 이메일 조건 : [대문자,소문자,숫자,특수기호] + 골뱅이(@) + [대문자,소문자,숫자,.,-] + 점(.) + [대문자,소문자]
+        let validEmail = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        let emailTest = NSPredicate(format: "SELF MATCHES %@", validEmail)
+          return emailTest.evaluate(with: input)
+    }
+    private func checkRepasswordRight (input: String) -> Bool {
+        // 이메일 조건 : [대문자,소문자,숫자,특수기호] + 골뱅이(@) + [대문자,소문자,숫자,.,-] + 점(.) + [대문자,소문자]
+        let validEmail = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        let emailTest = NSPredicate(format: "SELF MATCHES %@", validEmail)
+          return emailTest.evaluate(with: input)
     }
     
-    @objc private func checkRepasswordTextField() {
-        
-    }
+    
 
     // Alert
     func tempAlert(title: String, message: String) {
@@ -88,10 +92,26 @@ final class SignUpViewController: BaseViewController {
         present(alert, animated: true)
     }
     
+    // 조건에 따른 경고문구 숨김여부 결정
+    @objc private func checkEmailTextField() {
+        emailRight = checkEmailRight(input: emailTextField.text ?? "")
+        emailWarningStackView.isHidden = emailRight || !emailTextField.hasText ? true : false
+    }
+    
+    @objc private func checkPasswordTextField() {
+        
+    }
+    
+    @objc private func checkRepasswordTextField() {
+        
+    }
+    
+    // 버튼 활성화
     @objc func activateOkayButton(_ : UIButton) {
         okayButton.isEnabled = emailRight && passwordRight && rePasswordRight ? true : false
     }
     
+    // 회원가입 버튼 클릭
     @IBAction func touchUpToSignUp(_ sender: UIButton) {
         if passwordTextField.text == rePasswordTextField.text {
             tempAlert(title: "회원가입", message: "이메일 : \(emailTextField.text ?? ""), 비밀번호 : \(passwordTextField.text ?? "")")
