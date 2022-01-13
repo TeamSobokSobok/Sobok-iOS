ba99ad65a2a39d487695927131c6cfccea2ca1bc
chansenbbai <chanchu@hansung.ac.kr>
2022-01-14 00:00:43 +0900

    [Fix] SceneDelegate 수정

--- SobokSobok/SobokSobok/Application/SceneDelegate.swift
+++ SobokSobok/SobokSobok/Application/SceneDelegate.swift
@@ -15,7 +15,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
         
-        let sampleViewController = AddMedicineSecondViewController.instanceFromNib()
+        let sampleViewController = SampleViewController.instanceFromNib()
         window?.rootViewController = sampleViewController
         window?.makeKeyAndVisible()
     }
