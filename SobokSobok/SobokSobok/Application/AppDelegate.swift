1851ee97ed925cda4c520d01a2be29497b2c8559
seondal <sseondal@gmail.com>
2022-01-13 23:13:44 +0900

    [Feat] IQKeyboardManager 적용 (#39)

--- SobokSobok/SobokSobok/Application/AppDelegate.swift
+++ SobokSobok/SobokSobok/Application/AppDelegate.swift
@@ -6,14 +6,16 @@
 //
 
 import UIKit
+import IQKeyboardManagerSwift
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
+    
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        IQKeyboardManager.shared.enable = true
         return true
     }
 
