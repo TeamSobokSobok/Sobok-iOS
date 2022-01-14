aaa05ebaf797ebc548400ee60bf333759765ece3
seondal <sseondal@gmail.com>
2022-01-14 18:32:55 +0900

    [Add] SetNickNameVC (#48)

---
+++ SobokSobok/SobokSobok/Presentation/SignUp/SetNickNameVIewController.swift
@@ -0,0 +1,16 @@
+//
+//  SetNickNameVIewController.swift
+//  SobokSobok
+//
+//  Created by 김선오 on 2022/01/14.
+//
+
+import UIKit
+
+class SetNickNameVIewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
