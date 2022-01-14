a7a8dc05f0575d59bf773bd99356590867094abc
김태현 <taehyk528@gmail.com>
2022-01-15 00:27:56 +0900

    [Style] 스크롤뷰 영역 구현 (#53)

--- SobokSobok/SobokSobok/Presentation/Home/MainViewController.swift
+++ SobokSobok/SobokSobok/Presentation/Home/MainViewController.swift
@@ -7,23 +7,23 @@
 
 import UIKit
 
-class MainViewController: UIViewController {
+final class MainViewController: BaseViewController {
 
+    // MARK: - UI
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var scopeLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
 
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func style() {
+        titleLabel.setTypoStyle(typoStyle: .header1)
     }
-    */
-
+    
+    @IBAction func scopeButtonTapped(_ sender: Any) {
+    }
 }
