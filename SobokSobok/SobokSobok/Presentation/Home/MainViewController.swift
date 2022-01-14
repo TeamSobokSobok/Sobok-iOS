7e59036a13e5aab684ef4b22872aa016587a31b7
김태현 <taehyk528@gmail.com>
2022-01-15 00:13:38 +0900

    [Add] MainViewController (#53)

---
+++ SobokSobok/SobokSobok/Presentation/Home/MainViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MainViewController.swift
+//  SobokSobok
+//
+//  Created by taehy.k on 2022/01/15.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
