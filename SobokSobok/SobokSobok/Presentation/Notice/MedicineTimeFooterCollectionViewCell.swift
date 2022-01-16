09d993c7c8e30eb5d900838a3efafe5e57cdcd7e
EunHee-Jeong <18102007@seoultech.ac.kr>
2022-01-16 14:21:29 +0900

    [Add] 컬렉션뷰 셀 파일 생성 (#52)
    - MedicineTimeInfoCollectionViewCell
    - MedicineTimeFooterCollectionViewCell

---
+++ SobokSobok/SobokSobok/Presentation/Notice/MedicineTimeFooterCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  MedicineTimeFooterCollectionViewCell.swift
+//  SobokSobok
+//
+//  Created by 정은희 on 2022/01/16.
+//
+
+import UIKit
+
+class MedicineTimeFooterCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ SobokSobok/SobokSobok/Presentation/Notice/MedicineTimeInfoCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  MedicineTimeInfoCollectionViewCell.swift
+//  SobokSobok
+//
+//  Created by 정은희 on 2022/01/16.
+//
+
+import UIKit
+
+class MedicineTimeInfoCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
