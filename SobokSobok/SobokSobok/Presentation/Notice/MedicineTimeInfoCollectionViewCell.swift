76be027994bd4e5383edbb12591af6cf736fbbe3
EunHee-Jeong <18102007@seoultech.ac.kr>
2022-01-16 16:07:19 +0900

    [Style] 컬렉션뷰 셀 UI 구성 (#52)

--- SobokSobok/SobokSobok/Presentation/Notice/MedicineTimeInfoCollectionViewCell.swift
+++ SobokSobok/SobokSobok/Presentation/Notice/MedicineTimeInfoCollectionViewCell.swift
@@ -8,7 +8,11 @@
 import UIKit
 
 class MedicineTimeInfoCollectionViewCell: UICollectionViewCell {
-
+    
+    // MARK: - @IBOutlet Properties
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var deleteButton: UIButton!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
