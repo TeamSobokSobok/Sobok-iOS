e3f17d79c1e700689734c2cb3188e6c7ebec013b
EunHee-Jeong <18102007@seoultech.ac.kr>
2022-01-16 18:35:06 +0900

    [Add] 버튼 선택 시 올라올 화면 파일 추가 (#52)
    - WeekSelectViewController, TermSelectViewController

---
+++ SobokSobok/SobokSobok/Presentation/Notice/TermSelectViewController.swift
@@ -0,0 +1,15 @@
+//
+//  TermSelectViewController.swift
+//  SobokSobok
+//
+//  Created by 정은희 on 2022/01/16.
+//
+
+import UIKit
+
+class TermSelectViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ SobokSobok/SobokSobok/Presentation/Notice/WeekSelectViewController.swift
@@ -0,0 +1,15 @@
+//
+//  WeekSelectViewController.swift
+//  SobokSobok
+//
+//  Created by 정은희 on 2022/01/16.
+//
+
+import UIKit
+
+class WeekSelectViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
