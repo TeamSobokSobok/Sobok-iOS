2932dcdf616de224ae8851e031cab5c9d1aacaff
Taehyeon Kim <taehyk528@gmail.com>
2022-01-17 04:38:06 +0900

    Revert "[#55] 메인뷰 UI 디테일 작업"

--- SobokSobok/SobokSobok/Presentation/Home/Views/TimeHeaderView.swift
+++ SobokSobok/SobokSobok/Presentation/Home/Views/TimeHeaderView.swift
@@ -10,33 +10,14 @@ import UIKit
 final class TimeHeaderView: UICollectionReusableView {
     
     @IBOutlet weak var editButtonStackView: UIStackView!
-    @IBOutlet weak var editButton: UIButton!
     @IBOutlet weak var timeLabel: UILabel!
-    
-    var editState: Bool = false {
-        didSet {
-            
-        }
-    }
-    var editModeClosure: (() -> Void)?
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        style()
-    }
-    
-    private func style() {
-        editButton.setTitleColor(Color.gray700, for: .normal)
-        editButton.titleLabel?.setTypoStyle(typoStyle: .body5)
+        // Initialization code
     }
     
     override func prepareForReuse() {
         editButtonStackView.isHidden = false
-    }
-    
-    @IBAction func editButtonTapped(_ sender: Any) {
-        editState.toggle()
-        editButton.setTitle(editState ? "완료" : "수정", for: .normal)
-        editModeClosure?()
     }
 }
