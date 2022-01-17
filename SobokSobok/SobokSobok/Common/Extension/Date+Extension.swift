779d7217f0ac4748a07bd4242d5754d362dae623
김태현 <taehyk528@gmail.com>
2022-01-18 06:17:58 +0900

    [Add] Date 익스텐션 추가 (#85)

---
+++ SobokSobok/SobokSobok/Common/Extension/Date+Extension.swift
@@ -0,0 +1,30 @@
+//
+//  Date+Extension.swift
+//  SobokSobok
+//
+//  Created by taehy.k on 2022/01/18.
+//
+
+import Foundation
+
+extension Date {
+    enum FormatType {
+        case full
+        case year
+        case month
+        case time
+        
+        var description: String {
+            switch self {
+            case .full:
+                return ""
+            case .year:
+                <#code#>
+            case .month:
+                <#code#>
+            case .time:
+                <#code#>
+            }
+        }
+    }
+}
