e6b115485aca23c59837e902761cdfe0e7dd5a02
seondal <sseondal@gmail.com>
2022-01-18 21:15:45 +0900

    [Add] SignInDataModel (#91)

---
+++ SobokSobok/SobokSobok/Data/Model/SignInDataModel.swift
@@ -0,0 +1,14 @@
+//
+//  SignInDataModel.swift
+//  SobokSobok
+//
+//  Created by 김선오 on 2022/01/18.
+//
+
+import UIKit
+
+class SignInUser {
+    static let shared = SignInUser()
+    var email: String?
+    var password: String?
+}
