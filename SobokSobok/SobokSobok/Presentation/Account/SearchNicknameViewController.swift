a01d6e453984fabb2020f3e98dfe2c1e9a71e0dc
seondal <sseondal@gmail.com>
2022-01-19 01:44:18 +0900

    [Add] SearchNicknameVC (#101)

---
+++ SobokSobok/SobokSobok/Presentation/Account/SearchNicknameViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SearchNicknameViewController.swift
+//  SobokSobok
+//
+//  Created by 김선오 on 2022/01/19.
+//
+
+import UIKit
+
+class SearchNicknameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
