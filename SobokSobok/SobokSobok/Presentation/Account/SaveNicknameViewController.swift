a503e476f09816d90af1a5030e9181634419d1d0
seondal <sseondal@gmail.com>
2022-01-19 04:48:08 +0900

    [Style] SaveNicknameVC UI 구현 (#105)

--- SobokSobok/SobokSobok/Presentation/Account/SaveNicknameViewController.swift
+++ SobokSobok/SobokSobok/Presentation/Account/SaveNicknameViewController.swift
@@ -7,23 +7,32 @@
 
 import UIKit
 
-class SaveNicknameViewController: UIViewController {
+final class SaveNicknameViewController: BaseViewController {
 
+    @IBOutlet weak var nicknameTextLabel: UILabel!
+    @IBOutlet weak var searchView: UIView!
+    @IBOutlet weak var searchTextField: UITextField!
+    @IBOutlet weak var warningTextLabel: UILabel!
+    @IBOutlet weak var counterTextLabel: UILabel!
+    @IBOutlet weak var requestButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    override func style() {
+        // 초기세팅
+        searchView.makeRoundedWithBorder(radius: 12, color: Color.gray300.cgColor)
+        warningTextLabel.isHidden = true
+        counterTextLabel.isHidden = true
+        requestButton.isEnabled = false
     }
-    */
-
+    
+    @IBAction func touchUpToGoBack(_ sender: Any) {
+        navigationController?.popViewController(animated: true)
+    }
+    
+    @IBAction func touchUpToRequest(_ sender: Any) {
+        print("request")
+    }
 }
