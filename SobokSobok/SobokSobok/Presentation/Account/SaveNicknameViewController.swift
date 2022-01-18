758972bb74cc241861bf256ea92d46836ee57ae2
seondal <sseondal@gmail.com>
2022-01-19 06:05:09 +0900

    [Feat] 닉네임 형식 분기처리 (힘들었다..) (#105)

--- SobokSobok/SobokSobok/Presentation/Account/SaveNicknameViewController.swift
+++ SobokSobok/SobokSobok/Presentation/Account/SaveNicknameViewController.swift
@@ -26,43 +26,65 @@ final class SaveNicknameViewController: BaseViewController {
     }
     
     override func style() {
-        // 초기세팅
-        searchView.makeRoundedWithBorder(radius: 12, color: Color.gray300.cgColor)
-        warningTextLabel.isHidden = true
-        counterTextLabel.isHidden = true
-        requestButton.isEnabled = false
-        
-        // selector
+        noText()
         searchTextField.addTarget(self, action: #selector(self.checkTextField), for: .editingChanged)
     }
     
     // MARK: - Functions
     @objc private func checkTextField() {
-        // 글자여부 분기처리
-        if searchTextField.hasText {
-            setSearchTextFieldAvailable()
-        } else {
-            setSearchTextFieldUnavailable()
-        }
-        
-        // 글자수 제한
-        if searchTextField.text?.count ?? 0 > 10 {
-                searchTextField.deleteBackward()
-        }
-        
         // 글자수 카운트
         nameCount = searchTextField.text?.count ?? 0
         counterTextLabel.text = "\(nameCount) / 10"
+        
+        // 글자수 제한
+        if nameCount > 10 {
+            searchTextField.deleteBackward()
+        }
+        
+        // 조건 분기처리
+        if !searchTextField.hasText {
+            noText()
+        } else if !checkIsIncludeSpecial(input: searchTextField.text ?? "") {
+            warningTextLabel.text = "특수문자 입력은 불가능해요"
+            warn()
+        } else if nameCount < 2 {
+            warningTextLabel.text = "2자 이상 입력 가능해요"
+            warn()
+        } else {
+            right()
+        }
     }
     
-    // 글자 있을 때와 없을때 세팅
-    private func setSearchTextFieldAvailable () {
-        searchView.makeRoundedWithBorder(radius: 12, color: Color.gray600.cgColor)
-        counterTextLabel.isHidden = false
-    }
-    private func setSearchTextFieldUnavailable () {
+    // 글자가 없을 때
+    private func noText() {
         searchView.makeRoundedWithBorder(radius: 12, color: Color.gray300.cgColor)
         counterTextLabel.isHidden = true
+        warningTextLabel.isHidden = true
+        requestButton.isEnabled = false
+    }
+    
+    // 경고 세팅
+    private func warn() {
+        searchView.makeRoundedWithBorder(radius: 12, color: Color.pillColorRed.cgColor)
+        warningTextLabel.isHidden = false
+        counterTextLabel.isHidden = false
+        counterTextLabel.textColor = UIColor(cgColor: Color.pillColorRed.cgColor)
+        requestButton.isEnabled = false
+    }
+    
+    // 조건 맞을 때
+    private func right() {
+        searchView.makeRoundedWithBorder(radius: 12, color: Color.gray600.cgColor)
+        warningTextLabel.isHidden = true
+        counterTextLabel.textColor = UIColor(cgColor: Color.gray600.cgColor)
+        requestButton.isEnabled = true
+    }
+    
+    // 정규식 체크
+    private func checkIsIncludeSpecial (input: String) -> Bool {
+        let validNickName = "[A-Za-z가-힣0-9]{1,10}"
+        let nickNameTest = NSPredicate(format: "SELF MATCHES %@", validNickName)
+          return nickNameTest.evaluate(with: input)
     }
     
     // MARK: - @IBAction Properties
