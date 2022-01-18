57cc8a7b20522864ed1219ba4283937df124374c
seondal <sseondal@gmail.com>
2022-01-19 05:14:45 +0900

    [Feat] 텍스트필드 글자 수 카운트 (#105)

--- SobokSobok/SobokSobok/Presentation/Account/SaveNicknameViewController.swift
+++ SobokSobok/SobokSobok/Presentation/Account/SaveNicknameViewController.swift
@@ -9,6 +9,10 @@ import UIKit
 
 final class SaveNicknameViewController: BaseViewController {
 
+    // MARK: - Properties
+    private var nameCount: Int = 0
+    
+    // MARK: - @IBOutlet Properties
     @IBOutlet weak var nicknameTextLabel: UILabel!
     @IBOutlet weak var searchView: UIView!
     @IBOutlet weak var searchTextField: UITextField!
@@ -16,6 +20,7 @@ final class SaveNicknameViewController: BaseViewController {
     @IBOutlet weak var counterTextLabel: UILabel!
     @IBOutlet weak var requestButton: UIButton!
     
+    // MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -26,8 +31,41 @@ final class SaveNicknameViewController: BaseViewController {
         warningTextLabel.isHidden = true
         counterTextLabel.isHidden = true
         requestButton.isEnabled = false
+        
+        // selector
+        searchTextField.addTarget(self, action: #selector(self.checkTextField), for: .editingChanged)
     }
     
+    // MARK: - Functions
+    @objc private func checkTextField() {
+        // 글자여부 분기처리
+        if searchTextField.hasText {
+            setSearchTextFieldAvailable()
+        } else {
+            setSearchTextFieldUnavailable()
+        }
+        
+        // 글자수 제한
+        if searchTextField.text?.count ?? 0 > 10 {
+                searchTextField.deleteBackward()
+        }
+        
+        // 글자수 카운트
+        nameCount = searchTextField.text?.count ?? 0
+        counterTextLabel.text = "\(nameCount) / 10"
+    }
+    
+    // 글자 있을 때와 없을때 세팅
+    private func setSearchTextFieldAvailable () {
+        searchView.makeRoundedWithBorder(radius: 12, color: Color.gray600.cgColor)
+        counterTextLabel.isHidden = false
+    }
+    private func setSearchTextFieldUnavailable () {
+        searchView.makeRoundedWithBorder(radius: 12, color: Color.gray300.cgColor)
+        counterTextLabel.isHidden = true
+    }
+    
+    // MARK: - @IBAction Properties
     @IBAction func touchUpToGoBack(_ sender: Any) {
         navigationController?.popViewController(animated: true)
     }
