748c48164636e6af3f3e5b38035312a54a5d4d28
seondal <sseondal@gmail.com>
2022-01-19 02:48:18 +0900

    [Style] Add Properties (#101)

--- SobokSobok/SobokSobok/Presentation/Account/SearchNicknameViewController.swift
+++ SobokSobok/SobokSobok/Presentation/Account/SearchNicknameViewController.swift
@@ -7,23 +7,23 @@
 
 import UIKit
 
-class SearchNicknameViewController: UIViewController {
+final class SearchNicknameViewController: BaseViewController {
 
+    // MARK: - @IBOutlet Properties
+    @IBOutlet weak var closeButton: UIButton!
+    @IBOutlet weak var searchNicknameTextField: UITextField!
+    @IBOutlet weak var resultButton: UIButton!
+    @IBOutlet weak var resultTextLabel: UILabel!
+    @IBOutlet weak var noResultImageView: UIImageView!
+    
+    // MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
 
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - @IBAction Properties
+    
+    @IBAction func touchUpToAddFriend(_ sender: Any) {
+        print("적복이")
     }
-    */
-
 }
