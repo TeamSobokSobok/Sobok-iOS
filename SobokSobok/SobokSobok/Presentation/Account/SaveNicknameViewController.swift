3813c8570b18e92ebadb0edfd2ec3f669f0d65a2
seondal <sseondal@gmail.com>
2022-01-19 03:48:00 +0900

    [Add] SaveNickname (#105)

---
+++ SobokSobok/SobokSobok/Presentation/Account/SaveNicknameViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SaveNicknameViewController.swift
+//  SobokSobok
+//
+//  Created by 김선오 on 2022/01/19.
+//
+
+import UIKit
+
+class SaveNicknameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
