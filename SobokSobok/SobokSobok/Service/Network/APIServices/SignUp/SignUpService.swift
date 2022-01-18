4bcd8c7ff4a45e0a9025846a1bc72d50c6225bdd
seondal <sseondal@gmail.com>
2022-01-18 12:24:50 +0900

    [Add] SignUpService (#79)

---
+++ SobokSobok/SobokSobok/Service/Network/APIServices/SignUp/SignUpService.swift
@@ -0,0 +1,59 @@
+//
+//  SignUpService.swift
+//  SobokSobok
+//
+//  Created by 김선오 on 2022/01/18.
+//
+
+import Foundation
+import Moya
+
+enum SignUpService {
+    case signUp (email: String, password: String)
+    case checkUsername (nickname: String)
+}
+
+extension SignUpService: TargetType {
+    var baseURL: URL {
+        return URL(string: URLs.baseURL)!
+    }
+    
+    var path: String {
+        switch self {
+        case .signUp:
+            return URLs.signUpURL
+        case .checkUsername:
+            return URLs.checkUsernameURL
+        }
+    }
+    
+    var method: Moya.Method {
+        switch self {
+        case .signUp:
+            return .post
+        case .checkUsername:
+            return .post
+        }
+    }
+    
+    var sampleData: Data {
+        return Data()
+    }
+    
+    var task: Task {
+        switch self {
+        case .signUp(let email, let password):
+            return .requestParameters(parameters: ["email": email, "password": password], encoding: URLEncoding.queryString)
+        case .checkUsername(let nickname):
+            return .requestParameters(parameters: ["nickname": nickname], encoding: URLEncoding.queryString)
+        }
+    }
+    
+    var headers: [String: String]? {
+            return [
+                "Content-Type": "application/json"
+            ]
+        }
+    
+    
+}
