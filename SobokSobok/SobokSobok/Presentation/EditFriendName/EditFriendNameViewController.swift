1c9e73472ad7d751e0ba7a0715e6e370e2bf1613
seondal <sseondal@gmail.com>
2022-01-20 08:21:39 +0900

    [Style] layout() 초기세팅 (#121)

--- SobokSobok/SobokSobok/Presentation/EditFriendName/EditFriendNameViewController.swift
+++ SobokSobok/SobokSobok/Presentation/EditFriendName/EditFriendNameViewController.swift
@@ -7,23 +7,22 @@
 
 import UIKit
 
-class EditFriendNameViewController: UIViewController {
+final class EditFriendNameViewController: BaseViewController {
 
+    // MARK: - @IBOulet Properties
+    @IBOutlet weak var nameTextFieldView: UIView!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var warningTextLabel: UILabel!
+    @IBOutlet weak var counterTextLabel: UILabel!
+    
+    // MARK: - Life View Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    override func layout() {
+        nameTextFieldView.makeRoundedWithBorder(radius: 12, color: Color.gray600.cgColor)
+        warningTextLabel.isHidden = true
+        counterTextLabel.isHidden = true
     }
-    */
-
 }
