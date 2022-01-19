8adfd5090c66e3b17ee2a957c2f192e01be7b905
seondal <sseondal@gmail.com>
2022-01-20 02:02:46 +0900

    [Add] SaveNicknameModel (#117)

---
+++ SobokSobok/SobokSobok/Data/DTO/APIModel/Account/SaveNicknameModel.swift
@@ -0,0 +1,20 @@
+//
+//  SaveNicknameModel.swift
+//  SobokSobok
+//
+//  Created by 김선오 on 2022/01/20.
+//
+
+import Foundation
+
+// MARK: - DataClass
+struct SaveNicknameData {
+    let sendGroupID: Int?
+    let senderID: Int?
+    let senderName: String?
+    let memberID: Int?
+    let memberName: String?
+    let isSend: Bool?
+    let isOkay: NSNull?
+    let createdAt: String?
+}
