f05fec16aba6997f9bc6a1c8cac060eacb822482
seondal <sseondal@gmail.com>
2022-01-20 02:15:23 +0900

    [Add] AddAccountAPI saveNickname() 추가 (#117)

--- SobokSobok/SobokSobok/Data/DTO/APIModel/Account/SaveNicknameModel.swift
+++ SobokSobok/SobokSobok/Data/DTO/APIModel/Account/SaveNicknameModel.swift
@@ -7,14 +7,48 @@
 
 import Foundation
 
-// MARK: - DataClass
-struct SaveNicknameData {
-    let sendGroupID: Int?
-    let senderID: Int?
+// MARK: - SaveNicknameData
+struct SaveNicknameData: Codable {
+    let sendGroupID, senderID: Int?
     let senderName: String?
     let memberID: Int?
     let memberName: String?
     let isSend: Bool?
-    let isOkay: NSNull?
+    let isOkay: JSONNull?
     let createdAt: String?
+
+    enum CodingKeys: String, CodingKey {
+        case sendGroupID
+        case senderID
+        case senderName
+        case memberID
+        case memberName, isSend, isOkay, createdAt
+    }
+}
+
+// MARK: - Encode/decode helpers
+
+class JSONNull: Codable, Hashable {
+
+    public static func == (lhs: JSONNull, rhs: JSONNull) -> Bool {
+        return true
+    }
+
+    public var hashValue: Int {
+        return 0
+    }
+
+    public init() {}
+
+    public required init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        if !container.decodeNil() {
+            throw DecodingError.typeMismatch(JSONNull.self, DecodingError.Context(codingPath: decoder.codingPath, debugDescription: "Wrong type for JSONNull"))
+        }
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.singleValueContainer()
+        try container.encodeNil()
+    }
 }
