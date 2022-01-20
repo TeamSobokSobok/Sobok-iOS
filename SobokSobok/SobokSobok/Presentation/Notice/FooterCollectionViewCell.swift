e99d1bed69f0eb9a06c190949bd791b42eb087ed
EunHee-Jeong <18102007@seoultech.ac.kr>
2022-01-21 02:34:47 +0900

    [Edit] 디자인 QA 반영 (#90)
    - 푸터뷰 Label 행간 간격 조정해주는 코드 추가

--- SobokSobok/SobokSobok/Presentation/Notice/FooterCollectionViewCell.swift
+++ SobokSobok/SobokSobok/Presentation/Notice/FooterCollectionViewCell.swift
@@ -8,10 +8,32 @@
 import UIKit
 
 final class FooterCollectionViewCell: UICollectionViewCell {
-
+    
+    // MARK: - @IBOutlet Properties
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        setLabel()
     }
+    
+    func setLabel() {
+        descriptionLabel.setTextWithLineHeight(text: "전송받은 약이 내가 먹는 약과 다르다면\n수정해서 저장할 수 있어요", lineHeight: 21)
+    }
+}
 
+extension UILabel {
+    func setTextWithLineHeight(text: String?, lineHeight: CGFloat) {
+        if let text = text {
+            let style = NSMutableParagraphStyle()
+            style.maximumLineHeight = lineHeight
+            style.minimumLineHeight = lineHeight
+            let attributes: [NSAttributedString.Key: Any] = [
+                .paragraphStyle: style
+            ]
+            let attrString = NSAttributedString(string: text,
+                                                attributes: attributes)
+            self.attributedText = attrString
+        }
+    }
 }
