93242c48d5c0de2d06c5eeb80d5b742c722bbd87
seondal <sseondal@gmail.com>
2022-01-21 04:42:40 +0900

    [Add] .editFriendUsername in ShareService (#125)

--- SobokSobok/SobokSobok/Service/Network/APIServices/Share/ShareService.swift
+++ SobokSobok/SobokSobok/Service/Network/APIServices/Share/ShareService.swift
@@ -14,6 +14,7 @@ enum ShareService {
     case getGroupInfomation
     case getFriendCalendar(memberId: Int, date: String)
     case getFriendPillList(memberId: Int, date: String)
+    case editFriendUsername(groupId: Int, memberName: String)
 }
 
 extension ShareService: TargetType {
@@ -29,6 +30,9 @@ extension ShareService: TargetType {
             return URLs.getFriendCalendarURL.replacingOccurrences(of: "{memberId}", with: "\(memberId)")
         case .getFriendPillList(let memberId, _):
             return URLs.getFriendPillListURL.replacingOccurrences(of: "{memberId}", with: "\(memberId)")
+            
+        case .editFriendUsername(let groupId, _):
+            return URLs.editFriendUsernameURL.replacingOccurrences(of: "{groupId}", with: "\(groupId)")
         }
     }
 
@@ -36,6 +40,9 @@ extension ShareService: TargetType {
         switch self {
         case .getGroupInfomation, .getFriendCalendar, .getFriendPillList:
             return .get
+            
+        case .editFriendUsername:
+            return .put
         }
     }
 
@@ -45,12 +52,15 @@ extension ShareService: TargetType {
             return .requestPlain
         case .getFriendCalendar(_, let date), .getFriendPillList(_, let date):
             return .requestParameters(parameters: ["date": date], encoding: URLEncoding.queryString)
+            
+        case .editFriendUsername(_, let memberName):
+            return .requestParameters(parameters: ["memberName": memberName], encoding: JSONEncoding.default)
         }
     }
 
     var headers: [String: String]? {
         switch self {
-        case .getGroupInfomation, .getFriendCalendar, .getFriendPillList:
+        case .getGroupInfomation, .getFriendCalendar, .getFriendPillList, .editFriendUsername:
             return APIConstants.headerWithToken
         }
     }
