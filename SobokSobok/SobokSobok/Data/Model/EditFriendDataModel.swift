992b8fc1f5a35d378d2d92ebe25b7c19061a5cfd
seondal <sseondal@gmail.com>
2022-01-21 04:53:45 +0900

    [Add] EditFriendDataModel (#125)

---
+++ SobokSobok/SobokSobok/Data/Model/EditFriendDataModel.swift
@@ -0,0 +1,14 @@
+//
+//  EditFriendDataModel.swift
+//  SobokSobok
+//
+//  Created by 김선오 on 2022/01/21.
+//
+
+import UIKit
+
+class EditFriend {
+    static let shared = EditFriend()
+    var groupId: Int = 31
+    var memberName: String?
+}
