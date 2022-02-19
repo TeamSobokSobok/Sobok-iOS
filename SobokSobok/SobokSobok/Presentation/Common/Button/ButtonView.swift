b8b3194206e4dc6aecd0025189f2e285822d5b8a
chansenbbai <chanchu@hansung.ac.kr>
2022-02-19 15:13:38 +0900

    [#170] 버튼 모듈화
    ### Description
    
    - 소복소복 버튼을 보면 background 컬러로 나뉘어져 있어서 색으로 나눴습니다.
    - 매개변수에 mode, text, fontSize를 넣어 놨습니다. (자유롭게 사용해주세요)
    - cornerRadius를 우선 8로 잡아놨는데 이 부분은 나중에 수정해놓을게요
    - 예시 첨부해드립니다.
    
    Resolve #170

---
+++ SobokSobok/SobokSobok/Presentation/Common/Button/ButtonView.swift
@@ -0,0 +1,75 @@
+//
+//  ButtonView.swift
+//  SobokSobok
+//
+//  Created by 김승찬 on 2022/02/19.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class ButtonView: UIView {
+    
+    private lazy var button1 = ButtonStyle.init(frame: CGRect(), mode: .lightMint, text: "거절", fontSize: 13)
+    
+    private lazy var button2 = ButtonStyle.init(frame: CGRect(), mode: .mainMint, text: "수락", fontSize: 13)
+    
+    private lazy var button3 = ButtonStyle.init(frame: CGRect(), mode: .lightMint, text: "거절할래요", fontSize: 18)
+    
+    private lazy var button4 = ButtonStyle.init(frame: CGRect(), mode: .mainMint, text: "받을래요", fontSize: 18)
+    
+    private lazy var button5 = ButtonStyle.init(frame: CGRect(), mode: .mainMint, text: "선택 완료", fontSize: 18)
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUI()
+        setConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    private func setUI() {
+        [button1, button2, button3, button4, button5].forEach {
+            addSubview($0)
+        }
+    }
+    
+    private func setConstraints() {
+        button1.snp.makeConstraints {
+            $0.centerX.centerY.equalToSuperview()
+            $0.width.equalTo(145)
+            $0.height.equalTo(40)
+        }
+        
+        button2.snp.makeConstraints {
+            $0.top.equalTo(button1.snp.bottom).inset(-10)
+            $0.centerX.equalToSuperview()
+            $0.width.equalTo(145)
+            $0.height.equalTo(40)
+        }
+        
+        button3.snp.makeConstraints {
+            $0.top.equalTo(button2.snp.bottom).inset(-10)
+            $0.centerX.equalToSuperview()
+            $0.width.equalTo(162)
+            $0.height.equalTo(52)
+        }
+        
+        button4.snp.makeConstraints {
+            $0.top.equalTo(button3.snp.bottom).inset(-10)
+            $0.centerX.equalToSuperview()
+            $0.width.equalTo(162)
+            $0.height.equalTo(52)
+        }
+        
+        button5.snp.makeConstraints {
+            $0.top.equalTo(button4.snp.bottom).inset(-10)
+            $0.centerX.equalToSuperview()
+            $0.width.equalTo(335)
+            $0.height.equalTo(52)
+        }
+    }
+}
