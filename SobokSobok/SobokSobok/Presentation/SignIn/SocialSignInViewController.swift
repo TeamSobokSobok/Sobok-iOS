64d675c7dc3e413a77829779397aa491c883af70
seondal <sseondal@gmail.com>
2022-03-07 01:53:00 +0900

    [Create] SocialSignInVC

---
+++ SobokSobok/SobokSobok/Presentation/SignIn/SocialSignInViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SocialSignInViewController.swift
+//  SobokSobok
+//
+//  Created by 김선오 on 2022/03/07.
+//
+
+import UIKit
+
+class SocialSignInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
