23d440b7a70dc0d1e1e7820fc9e2d4de97682f02
chansenbbai <chanchu@hansung.ac.kr>
2022-03-14 23:09:26 +0900

    [#179] TimeViewModel

--- SobokSobok/SobokSobok/Presentation/Pill/PillFirst/ViewModel/PillTimeViewModel.swift
+++ SobokSobok/SobokSobok/Presentation/Pill/PillFirst/ViewModel/PillTimeViewModel.swift
@@ -8,7 +8,27 @@
 import RxCocoa
 import RxSwift
 
-struct PillTimeViewModel {
+final class PillTimeViewModel: ViewModelType {
     // 추후 추가 예정 !!!!
+    
+    struct Input {
+        
+    }
+    
+    struct Output {
+        
+    }
+    
+    let disposeBag = DisposeBag()
     let pillTimeList = Observable.of(["오전 8:00", "오후 1:00", "오후 7:00"])
+    lazy var prelay = BehaviorRelay<String>(value: "ㅎㅇㅎㅇ")
+    
+    let cellData: Driver<[Time]>
+    let pop: Signal<Void>
+    let itemSelected = PublishRelay<Int>()
+    
+    let selectedCategory = PublishSubject<Time>()
+    
+    
 }
+
