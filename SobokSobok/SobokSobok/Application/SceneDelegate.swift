274b874e035e8ad175151f5aa81d9ec2932433ac
seondal <sseondal@gmail.com>
2022-03-23 00:17:32 +0900

    Update SceneDelegate.swift

--- SobokSobok/SobokSobok/Application/SceneDelegate.swift
+++ SobokSobok/SobokSobok/Application/SceneDelegate.swift
@@ -15,7 +15,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
         
-        window?.rootViewController = UINavigationController(rootViewController: SetNickNameVIewController.instanceFromNib())
+        window?.rootViewController = UINavigationController(rootViewController: SplashViewController.instanceFromNib())
         window?.makeKeyAndVisible()
     }
 }
