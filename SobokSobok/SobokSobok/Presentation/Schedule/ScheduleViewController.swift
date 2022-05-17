33e7020c422b464f1db8dd53f0b4898ea3fee719
김태현 <taehyk528@gmail.com>
2022-05-18 00:07:10 +0900

    [Add] ScheduleViewController.swift

---
+++ SobokSobok/SobokSobok/Presentation/Schedule/ScheduleViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ScheduleViewController.swift
+//  SobokSobok
+//
+//  Created by taehy.k on 2022/05/18.
+//
+
+import UIKit
+
+final class ScheduleViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
