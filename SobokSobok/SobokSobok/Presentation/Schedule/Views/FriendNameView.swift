1f78c4684e7d80cb294a895945ddd71cb6b63743
김태현 <taehyk528@gmail.com>
2022-06-20 19:27:55 +0900

    [Feat] FriendNameView UI 구현

--- SobokSobok/SobokSobok/Presentation/Schedule/Views/FriendNameView.swift
+++ SobokSobok/SobokSobok/Presentation/Schedule/Views/FriendNameView.swift
@@ -5,4 +5,31 @@
 //  Created by taehy.k on 2022/06/20.
 //
 
-import Foundation
+import UIKit
+
+final class FriendNameView: BaseView {
+    
+    lazy var friendNameLabel = UILabel().then {
+        $0.font = UIFont.font(.pretendardBold, ofSize: 24)
+    }
+    
+    lazy var friendNameEditButton = UIButton().then {
+        $0.setImage(Image.icPencil32, for: .normal)
+        $0.tintColor = Color.gray400
+    }
+    
+    override func setupView() {
+        addSubviews(friendNameLabel, friendNameEditButton)
+    }
+    
+    override func setupConstraints() {
+        friendNameLabel.snp.makeConstraints {
+            $0.top.bottom.equalToSuperview()
+            $0.leading.equalToSuperview().offset(20)
+        }
+        friendNameEditButton.snp.makeConstraints {
+            $0.leading.equalTo(friendNameLabel.snp.trailing)
+            $0.bottom.equalTo(friendNameLabel.snp.bottom)
+        }
+    }
+}
