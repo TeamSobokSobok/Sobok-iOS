42bdf5ad49daba1cd9d39a7d2adad9258e356664
김태현 <taehyk528@gmail.com>
2022-06-20 20:45:54 +0900

    [Feat] MainMessageView UI 구현

--- SobokSobok/SobokSobok/Presentation/Schedule/Views/MainMessageView.swift
+++ SobokSobok/SobokSobok/Presentation/Schedule/Views/MainMessageView.swift
@@ -8,6 +8,30 @@
 import UIKit
 
 final class MainMessageView: BaseView {
-
+    
+    let mainMessageLabel = UILabel().then {
+//        $0.setTypoStyle(typoStyle: .header1)
+        $0.font = UIFont.font(.pretendardBold, ofSize: 24)
+        $0.numberOfLines = 2
+    }
+    
+    lazy var settingButton = UIButton().then {
+        $0.setImage(Image.icMenu48, for: .normal)
+    }
+    
+    override func setupView() {
+        addSubviews(mainMessageLabel, settingButton)
+    }
+    
+    override func setupConstraints() {
+        mainMessageLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(12)
+            $0.leading.equalToSuperview().offset(20)
+            $0.bottom.equalToSuperview().inset(40)
+        }
+        settingButton.snp.makeConstraints {
+            $0.top.equalToSuperview()
+            $0.trailing.equalToSuperview().inset(7)
+        }
+    }
 }
-
