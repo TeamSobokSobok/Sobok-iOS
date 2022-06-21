7fe1b868319919aca0e0bbfd9182a4b0b91d340c
김태현 <taehyk528@gmail.com>
2022-06-22 02:04:14 +0900

    [Edit] DataSource 코드 분리

--- SobokSobok/SobokSobok/Presentation/Schedule/Controllers/ScheduleViewController+DataSource.swift
+++ SobokSobok/SobokSobok/Presentation/Schedule/Controllers/ScheduleViewController+DataSource.swift
@@ -5,4 +5,37 @@
 //  Created by taehy.k on 2022/06/22.
 //
 
-import Foundation
+import UIKit
+
+// MARK: - UICollectionViewDataSource
+
+extension ScheduleViewController: UICollectionViewDataSource {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 3
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(
+            withReuseIdentifier: ScheduleCell.reuseIdentifier,
+            for: indexPath
+        ) as? ScheduleCell else { return UICollectionViewCell() }
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        guard let headerView = collectionView.dequeueReusableSupplementaryView(
+            ofKind: kind,
+            withReuseIdentifier: ScheduleHeaderView.reuseIdentifier,
+            for: indexPath
+        ) as? ScheduleHeaderView else { return UICollectionReusableView() }
+        
+        if indexPath.section != 0 { headerView.hideEditButton() }
+        
+        return headerView
+    }
+}
