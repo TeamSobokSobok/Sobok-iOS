b12e3b9b8e8d603a9678bc635bd548fca82d1d50
김태현 <taehyk528@gmail.com>
2022-06-21 23:19:49 +0900

    [Feat] ScheduleHeaderView UI 구현

--- SobokSobok/SobokSobok/Presentation/Schedule/Views/ScheduleHeaderView.swift
+++ SobokSobok/SobokSobok/Presentation/Schedule/Views/ScheduleHeaderView.swift
@@ -7,6 +7,51 @@
 
 import UIKit
 
-final class ScheduleHeaderView: BaseView {
-
+final class ScheduleHeaderView: UICollectionReusableView {
+    private let timeLabel = UILabel().then {
+        $0.text = "오후 1:00"
+    }
+    
+    private lazy var editButton = UIButton().then {
+        $0.setTitle("수정", for: .normal)
+        $0.setTitleColor(Color.gray700, for: .normal)
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureUI()
+        configureView()
+        configureConstraints()
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    private func configureView() {
+        addSubviews(timeLabel, editButton)
+    }
+    
+    private func configureConstraints() {
+        timeLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(36)
+            $0.leading.equalToSuperview().offset(20)
+            $0.bottom.equalToSuperview().inset(6)
+        }
+        editButton.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(19)
+            $0.trailing.equalToSuperview().inset(20)
+            $0.height.equalTo(23)
+        }
+    }
+    
+    private func configureUI() {
+        timeLabel.setTypoStyle(typoStyle: .title1)
+        editButton.titleLabel?.setTypoStyle(typoStyle: .body5)
+    }
+    
+    func hideEditButton() {
+        editButton.isHidden = true
+    }
 }
