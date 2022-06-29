fdcbb280cd38d13efc1a7e67c5485d954ea543c2
김태현 <taehyk528@gmail.com>
2022-06-29 14:45:00 +0900

    [Feat] APIClient Request 코드 작성

--- SobokSobok/SobokSobok/Network/Foundation/APIClient.swift
+++ SobokSobok/SobokSobok/Network/Foundation/APIClient.swift
@@ -11,6 +11,24 @@ protocol Requestable: AnyObject {
     func request<T: Decodable>(_ request: NetworkRequest) async throws -> T?
 }
 
-final class APIClient {
-    
+final class APIClient: Requestable {
+    func request<T: Decodable>(_ request: NetworkRequest) async throws -> T? {
+        guard let encodedURL = request.url.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed),
+              let url = URL(string: encodedURL) else {
+            throw APIError.urlEncodingError
+        }
+        
+        let (data, response) = try await URLSession.shared.data(for: request.createURLRequest(with: url))
+        guard let httpResponse = response as? HTTPURLResponse,
+              (200..<500) ~= httpResponse.statusCode else {
+            throw APIError.serverError
+        }
+        
+        let decodedData = try JSONDecoder().decode(BaseModel<T>.self, from: data)
+        if decodedData.success {
+            return decodedData.data
+        } else {
+            throw APIError.clientError(message: decodedData.message)
+        }
+    }
 }
