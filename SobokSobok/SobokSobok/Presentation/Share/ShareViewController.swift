0f034b0dafcf66351c43c9fdc434f37807e70804
김태현 <taehyk528@gmail.com>
2022-08-02 22:06:19 +0900

    [Feat] 그룹 정보 API 연결

--- SobokSobok/SobokSobok/Presentation/Share/ShareViewController.swift
+++ SobokSobok/SobokSobok/Presentation/Share/ShareViewController.swift
@@ -8,20 +8,17 @@
 import UIKit
 
 final class ShareViewController: BaseViewController {
-    var members: [Member] = [
-        Member(groupId: 12, memberId: 12, memberName: "TAB"),
-        Member(groupId: 12, memberId: 12, memberName: "TAB"),
-        Member(groupId: 12, memberId: 12, memberName: "TAB"),
-    ]
+    var members: [Member] = []
+    lazy var scheduleManager: ScheduleServiceable = ScheduleManager(apiService: APIManager(),
+                                                               environment: .development)
     
-    private lazy var shareTopView = ShareTopView()
+    lazy var shareTopView = ShareTopView()
     private let scheduleViewController = ScheduleViewController()
     private lazy var containerView = UIView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        getGroupInfo()
-        
+
         view.addSubviews(shareTopView, containerView)
         embed(scheduleViewController, inView: containerView)
         
@@ -42,27 +39,6 @@ final class ShareViewController: BaseViewController {
         super.viewWillAppear(animated)      
 
         tabBarController?.tabBar.isHidden = false
-    }
-}
-
-extension ShareViewController: PagerTabDelegate {
-    func addFriendDeleagte() {
-        let viewController = SearchNicknameViewController.instanceFromNib()
-        navigationController?.pushViewController(viewController, animated: true)
-    }
-}
-
-extension ShareViewController {
-    private func getGroupInfo() {
-        ShareAPI.shared.getGroupInfo { response in
-            switch response {
-            case .success(let data):
-                if let data = data as? [Member] {
-                    self.members = data
-                }
-            default:
-                return
-            }
-        }
+        getGroupInformation()
     }
 }
