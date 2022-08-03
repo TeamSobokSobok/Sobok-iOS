3e68d2330420e38a8840a8c3a448239f1107f8c4
seondal <sseondal@gmail.com>
2022-08-03 18:31:24 +0900

    [Feat] 회원탈퇴뷰 텍스트뷰 placeholder 기능 구현 (#237)

--- SobokSobok/SobokSobok/Presentation/MyInfo/Settings/WithdrawalViewController.swift
+++ SobokSobok/SobokSobok/Presentation/MyInfo/Settings/WithdrawalViewController.swift
@@ -7,10 +7,9 @@
 
 import UIKit
 
-protocol WitrhdrawalProtocol: StyleProtocol {}
+protocol WitrhdrawalProtocol: StyleProtocol, DelegationProtocol {}
 
 final class WithdrawalViewController: UIViewController, WitrhdrawalProtocol {
-
     @IBOutlet weak var reasonView: UIView!
     @IBOutlet weak var reasonTextView: UITextView!
     @IBOutlet weak var reasonTextCounter: UILabel!
@@ -19,11 +18,16 @@ final class WithdrawalViewController: UIViewController, WitrhdrawalProtocol {
     override func viewDidLoad() {
         super.viewDidLoad()
         style()
+        assignDelegation()
     }
     
     func style () {
         reasonView.makeRoundedWithBorder(radius: 12, color: Color.gray300.cgColor)
         witrhdrawButton.makeRounded(radius: 12)
+    }
+    
+    func assignDelegation() {
+        reasonTextView.delegate = self
     }
     
     @IBAction func backToSettingVC(_ sender: UIButton) {
@@ -32,5 +36,21 @@ final class WithdrawalViewController: UIViewController, WitrhdrawalProtocol {
     
     @IBAction func confirmWitrhdrawal(_ sender: UIButton) {
         print("회원탈퇴")
+    }
+}
+
+extension WithdrawalViewController: UITextViewDelegate {
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        if(textView.textColor == UIColor(cgColor: Color.gray500.cgColor)) {
+            textView.text = ""
+            textView.textColor = UIColor(cgColor: Color.black.cgColor)
+        }
+    }
+    
+    func textViewDidEndEditing(_ textView: UITextView) {
+        if textView.text.isEmpty {
+            textView.text = "적어주시는 여러분의 소중한 의견은 서비스 개선에 큰 도움이 되어요:)"
+            textView.textColor = UIColor(cgColor: Color.gray500.cgColor)
+        }
     }
 }
