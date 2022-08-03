b3aabbd749d300b5b61ab25cfd86bb5598d61608
김태현 <taehyk528@gmail.com>
2022-08-03 20:41:55 +0900

    [Feat] 받은 스티커 모두 확인 API EndPoint, Service 추가

--- SobokSobok/SobokSobok/Network/API/StickerEndPoint.swift
+++ SobokSobok/SobokSobok/Network/API/StickerEndPoint.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+
+enum StickerEndPoint {
+    case getStickers(scheduleId: Int)
+}
+
+extension StickerEndPoint: EndPoint {
+    var method: HttpMethod {
+        switch self {
+        case .getStickers:
+            return .GET
+        }
+    }
+    
+    var body: Data? {
+        switch self {
+        case .getStickers:
+            return nil
+        }
+    }
+    
+    func getURL(from environment: APIEnvironment) -> String {
+        let baseURL = environment.baseURL
+        switch self {
+        case .getStickers(let scheduleId):
+            return "\(baseURL)/sticker/\(scheduleId)"
+        }
+    }
+}
--- SobokSobok/SobokSobok/Network/Service/StickerManager.swift
+++ SobokSobok/SobokSobok/Network/Service/StickerManager.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+
+protocol StickerServiceable {
+    func getStickers(for scheduleId: Int) async throws -> [Stickers]?
+}
+
+struct StickerManager: StickerServiceable {
+    private let apiService: Requestable
+    private let environment: APIEnvironment
+    
+    init(apiService: Requestable, environment: APIEnvironment) {
+        self.apiService = apiService
+        self.environment = environment
+    }
+    
+    func getStickers(for scheduleId: Int) async throws -> [Stickers]? {
+        let request = StickerEndPoint
+            .getStickers(scheduleId: scheduleId)
+            .createRequest(environment: environment)
+        return try await self.apiService.request(request)
+    }
+}
