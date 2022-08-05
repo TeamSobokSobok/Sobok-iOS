749471851a25bf6e6e3c2abe041fff379e9c8ced
EunHee-Jeong <18102007@seoultech.ac.kr>
2022-08-05 16:48:28 +0900

    [Add] NoticeFirst API 모델 생성 (#202)
    - 레거시 파일 제거

---
+++ SobokSobok/SobokSobok/Data/DTO/APIModel/Notice/NoticeListModel.swift
@@ -0,0 +1,23 @@
+//
+//  NoticeListModel.swift
+//  SobokSobok
+//
+//  Created by 정은희 on 2022/08/05.
+//
+
+import Foundation
+
+struct NoticeList: Codable {
+    let infoList: [InfoList]?
+}
+
+struct InfoList: Codable {
+    let noticeId: Int
+    let section: String
+    let isOkay: String
+    let isSend: Bool
+    let createdAt: String
+    let senderName: String
+    let pillName: String?
+    let pillId: Int?
+}
