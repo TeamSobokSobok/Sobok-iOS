66b76ca5fe9c5864a0ba140f2546b74c48bb5c19
김태현 <taehyk528@gmail.com>
2022-08-07 18:50:39 +0900

    [Edit] Date extension 수정

--- SobokSobok/SobokSobok/Common/Extension/Date+Extension.swift
+++ SobokSobok/SobokSobok/Common/Extension/Date+Extension.swift
@@ -7,43 +7,54 @@
 
 import Foundation
 
-extension Date {
-    enum FormatType {
-        case full
-        case year
-        case day
-        case month
-        case second
-        case time
-        case calendar
-        case calendarTime
-        
-        var description: String {
-            switch self {
-            case .full:
-                return "yyyy-MM-dd'T'HH:mm:ss.SSS'Z"
-            case .year:
-                return "yyyy-MM-dd"
-            case .day:
-                return "M월 d일 EEEE"
-            case .month:
-                return "M월"
-            case .second:
-                return "HH:mm:ss"
-            case .time:
-                return "a h:mm"
-            case .calendar:
-                return "yyyy년 MM월 dd일"
-            case .calendarTime:
-                return "a HH:mm"
-            }
+enum FormatType {
+    case full
+    case year
+    case day
+    case month
+    case second
+    case time
+    case calendar
+    case calendarTime
+    
+    var description: String {
+        switch self {
+        case .full:
+            return "yyyy-MM-dd'T'HH:mm:ss.SSS'Z"
+        case .year:
+            return "yyyy-MM-dd"
+        case .day:
+            return "M월 d일 EEEE"
+        case .month:
+            return "M월"
+        case .second:
+            return "HH:mm:ss"
+        case .time:
+            return "a h:mm"
+        case .calendar:
+            return "yyyy년 MM월 dd일"
+        case .calendarTime:
+            return "a HH:mm"
         }
     }
+}
+
+extension Date {
     
     func toString(of type: FormatType) -> String {
         let dateFormatter = DateFormatter()
         dateFormatter.locale = Locale(identifier: "ko_KR")
         dateFormatter.dateFormat = type.description
         return dateFormatter.string(from: self)
+    }
+}
+
+extension String {
+    
+    func toDate(to type: FormatType) -> Date? {
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = Locale(identifier: "ko_KR")
+        dateFormatter.dateFormat = type.description
+        return dateFormatter.date(from: self)
     }
 }
