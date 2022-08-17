652087837b9647f12b8dd6079241dacc70113851
chansenbbai <chanchu@hansung.ac.kr>
2022-08-17 20:24:05 +0900

    [#290] 캘린더뷰 수정

--- SobokSobok/SobokSobok/Presentation/Pill/Me/PillSecond/ViewController/AddPillSecondViewController.swift
+++ SobokSobok/SobokSobok/Presentation/Pill/Me/PillSecond/ViewController/AddPillSecondViewController.swift
@@ -7,13 +7,18 @@
 
 import UIKit
 
-protocol AddPillSecondProtocol: StyleProtocol, TargetProtocol, TossPillProtocol {}
+import RxCocoa
+import RxSwift
+
+protocol AddPillSecondProtocol: StyleProtocol, TargetProtocol, TossPillProtocol, CalendarViewDelegate {}
 
 final class AddPillSecondViewController: UIViewController, AddPillSecondProtocol {
+   
    
     var type: TossPill = .myPill
     private let addPillSecondView = AddPillSecondView()
     private let sendPillViewModel: SendPillViewModel
+    private let disposeBag = DisposeBag()
     
     init(sendPillViewModel: SendPillViewModel) {
         self.sendPillViewModel = sendPillViewModel
@@ -31,13 +36,64 @@ final class AddPillSecondViewController: UIViewController, AddPillSecondProtocol
     override func viewDidLoad() {
         super.viewDidLoad()
         target()
-        print(sendPillViewModel.takeInterval)
-        print(sendPillViewModel.time)
-        print(sendPillViewModel.day)
         
-        print(sendPillViewModel.specific)
-        print(sendPillViewModel.specific.changeKrToEn())
+        addPillSecondView.calendar.delegate = self
+        
+        let date = Date()
+        
+        guard let date1 = Calendar.current.date(byAdding: .day, value: 92, to: date) else { return }
+
+        self.addPillSecondView.pillPeriodLabel.text = "\(date.toString(of: .year)) ~ \(date1.toString(of: .year))"
+        
+        self.addPillSecondView.checkBoxButton.rx.tap.bind {
+            self.addPillSecondView.checkBoxButton.isSelected.toggle()
+             
+            if self.addPillSecondView.checkBoxButton.isSelected {
+                self.addPillSecondView.checkBoxButton.setImage(Image.icCheckButton48, for: .normal)
+                self.unableNextButton()
+            } else {
+                self.addPillSecondView.checkBoxButton.setImage(Image.icChecked, for: .normal)
+                self.enableNextButton()
+            }
+
+            self.addPillSecondView.calendar.calculateThreeMonthRange(isCheckedThreeMonthState: self.addPillSecondView.checkBoxButton.isSelected)
+      
+        }
+        .disposed(by: disposeBag)
+        
+        
     }
+    
+    func didSelectFirstDate(_ calendar: CalendarView) {
+        
+        print("select")
+        
+        if self.addPillSecondView.calendar.firstDate == nil {
+            
+            self.addPillSecondView.checkBoxButton.setImage(Image.icCheckButton48, for: .normal)
+            self.unableNextButton()
+        }
+        
+        guard let calendar = calendar.firstDate else { return }
+        
+        self.sendPillViewModel.start = calendar.toString(of: .year)
+        
+        self.addPillSecondView.pillPeriodLabel.text = "\(sendPillViewModel.start) ~ \(sendPillViewModel.end)"
+     
+    }
+    
+    func didSelectLastDate(_ calendar: CalendarView) {
+        guard let calendar = calendar.lastDate else { return }
+        
+        self.sendPillViewModel.end = calendar.toString(of: .year)
+    
+        self.addPillSecondView.pillPeriodLabel.text = "\(sendPillViewModel.start) ~ \(sendPillViewModel.end)"
+        
+        if self.addPillSecondView.calendar.firstDate != nil && self.addPillSecondView.calendar.lastDate != nil {
+            self.enableNextButton()
+        }
+    }
+    
 
     func target() {
         addPillSecondView.nextButton.addTarget(self, action: #selector(pushAddPillThirdView), for: .touchUpInside)
@@ -52,9 +108,13 @@ final class AddPillSecondViewController: UIViewController, AddPillSecondProtocol
         }
     }
     
-    private func pushViewController(style: PillStyle) {
-        let addPillThirdView = AddPillThirdViewController(sendPillViewModel: SendPillViewModel(), pillThirdViewModel: PillThirdViewModel())
+    private func pushViewController(style: PillStyle) {        
+        
+        let addPillThirdView = AddPillThirdViewController(sendPillViewModel: sendPillViewModel, pillThirdViewModel: PillThirdViewModel())
+        
+        
         addPillThirdView.divide(style: style)
+        addPillThirdView.type = style.type
         self.navigationController?.pushViewController(addPillThirdView, animated: true)
     }
     
@@ -76,6 +136,18 @@ final class AddPillSecondViewController: UIViewController, AddPillSecondProtocol
         }
         
         navigationView.navigationTitleLabel.text = style.navigationTitle
+    }
+    
+    private func enableNextButton() {
+        addPillSecondView.nextButton.backgroundColor = Color.mint
+        addPillSecondView.nextButton.setTitleColor(Color.white, for: .normal)
+        addPillSecondView.nextButton.isEnabled = true
+    }
+    
+    private func unableNextButton() {
+        addPillSecondView.nextButton.backgroundColor = Color.gray200
+        addPillSecondView.nextButton.setTitleColor(Color.gray500, for: .normal)
+        addPillSecondView.nextButton.isEnabled = false
     }
     
     func style() {
